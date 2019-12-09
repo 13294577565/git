8dcbfc51c13cf880240d26c40bb2029e8e9c07e8
jiajinyu <a13294577565@163.com>
2019-12-09 08:46:23 +0800

    jiajinyu

--- app1/urls.py
+++ app1/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.aa,name='aa'),
-    path('login/',views.login ,name='login')
-
-
+    path('login/',views.login ,name='login'),
+    path('index/',views.index,name='index')
 ]
